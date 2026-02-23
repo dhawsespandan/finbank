23ba8471a1746ab1394cb7e0266170a60572984a
Spandan <dhawsespandan@gmail.com>
2026-02-23 11:45:52 +0530

    fix cors vercel url

--- src/main/java/com/finbank/config/SecurityConfig.java
+++ src/main/java/com/finbank/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://finbank-frontend.vercel.app"
+                "https://finbank-eta.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
